6d4637e221f3259810c220988b768746de500a10
Vitorrodrys <Vitorrodrys@gmail.com>
2024-08-13 19:17:02 -0300

    Fix command registry to default value as None

--- server/session_handler/api_session/command_registry.py
+++ server/session_handler/api_session/command_registry.py
@@ -1,16 +1,19 @@
 from inspect import signature
 from functools import wraps
-from typing import Callable
+from typing import Callable, Optional
 
 class CommandRegistry:
     def __init__(self):
-        self.command_map:dict[str, Callable] = {}
+        self.command_map: dict[str, Callable] = {}
 
-    def add(self, command_name)->Callable:
+    def add(self, command_name: str) -> Callable:
         """
         Provides a decorator to register a function with a command name.
         """
-        def decorator(func:Callable):
+        def decorator(func: Callable):
+
+            if command_name in self.command_map:
+                raise ValueError(f"Command '{command_name}' is already registered.")
 
             @wraps(func)
             def wrapper(*args, **kwargs):
@@ -20,11 +23,11 @@ class CommandRegistry:
                 for name, value in bound_values.items():
                     if name in sig.parameters:
                         param = sig.parameters[name]
-                        if param.annotation != param.empty:
+                        if param.annotation != param.empty and value is not None:
                             try:
                                 bound_values[name] = param.annotation(value)
-                            except ValueError as e:
-                                raise TypeError(f"Failed to convert parameter '{name}' to {param.annotation}") from e
+                            except (ValueError, TypeError) as e:
+                                raise TypeError(f"Failed to convert parameter '{name}' to {param.annotation}: {value}") from e
 
                 return func(*bound_values.values())
             self.command_map[command_name] = wrapper
@@ -32,7 +35,7 @@ class CommandRegistry:
 
         return decorator
 
-    def get_command(self, command_name:str)->Callable:
+    def get_command(self, command_name: str) -> Optional[Callable]:
         """
         Return the specified command function mapped to the command name.
         """
