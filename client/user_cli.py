1552baa9e6d3293099d109711afbbc56431c01e4
Vitorrodrys <Vitorrodrys@gmail.com>
2024-08-13 16:36:09 -0300

    Sync pause too em stream layer thread

--- client/user_cli.py
+++ client/user_cli.py
@@ -13,17 +13,19 @@ def starts_user_session()->tuple[UserAPI, StreamLayer, int]:
         break
     return user_api, stream_layer, video_size
 
-def interactive_seek(user_api: UserAPI, video_size: int):
+def interactive_seek(user_api: UserAPI, stream_layer:StreamLayer, video_size: int):
     percentage_video = float(input("percentage video to seek: "))/100
     offset = int(video_size * percentage_video)
-    user_api.seek(offset)
+    with stream_layer.get_lock():
+        user_api.seek(offset)
+def interactive_pause(user_api: UserAPI, stream_layer: StreamLayer):
+    stream_layer.pause()
+    user_api.pause()
 
-def interactive_cli():
-    user_api, stream_layer, video_size = starts_user_session()
-
+def _interactive_cli(user_api: UserAPI, stream_layer: StreamLayer, video_size: int):
     commands = {
-        "pause": user_api.pause,
-        "seek":  lambda: interactive_seek(user_api, video_size),
+        "pause": lambda: interactive_pause(user_api, stream_layer),
+        "seek":  lambda: interactive_seek(user_api, stream_layer, video_size),
         "quit":  user_api.stop
     }
     command = input("type a command: ")
@@ -34,3 +36,12 @@ def interactive_cli():
     user_api.stop()
     stream_layer.stop()
     print("session closed")
+
+def interactive_cli():
+    user_api, stream_layer, video_size = starts_user_session()
+    try:
+        _interactive_cli(user_api, stream_layer, video_size)
+    finally:
+        user_api.stop()
+        stream_layer.stop()
+        print("session closed")
